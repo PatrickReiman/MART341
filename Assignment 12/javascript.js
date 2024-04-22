5d3bc6c3936010e0f15522a7493a7e71df8dcf0a
PatrickReiman <patrickreiman122@gmail.com>
2024-04-21 23:56:38 -0600

    patch

--- Assignment 12/javascript.js
+++
@@ -1,5 +0,0 @@
-// Function for playing and making the video visible upon button click
-function playVideo() {
-    document.getElementById("video").play();
-    document.getElementById("video").style.visibility = "visible";
-}
