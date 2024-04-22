fb31c0da262dac62dbfd3fe0e470a7ec14735932
PatrickReiman <patrickreiman122@gmail.com>
2024-04-21 23:31:24 -0600

    HW 12

---
+++ Assignment 12/javascript.js
@@ -0,0 +1,5 @@
+// Function for playing and making the video visible upon button click
+function playVideo() {
+    document.getElementById("video").play();
+    document.getElementById("video").style.visibility = "visible";
+}
